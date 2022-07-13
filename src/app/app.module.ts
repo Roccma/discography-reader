d340a2673dd97c46b7aa16783fa2d9cee23545bb
Mauro Rocca <mauroroccaf@gmail.com>
2022-07-13 16:34:10 -0300

    build(general): working on ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BoardComponent } from './pages/board/board.component';
+
+import { TrelloCredentialsComponent } from './components/trello-credentials/trello-credentials.component';
+import { BoardSettingsComponent } from './components/board-settings/board-settings.component';
+import { DataReaderComponent } from './components/data-reader/data-reader.component';
+import { FormsModule } from '@angular/forms';
+import { ResultComponent } from './components/result/result.component';
+import { RouterModule } from '@angular/router';
+import { DiskCardComponent } from './components/disk-card/disk-card.component';
+import { DecadeListComponent } from './components/decade-list/decade-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    BoardComponent,
+    TrelloCredentialsComponent,
+    BoardSettingsComponent,
+    DataReaderComponent,
+    ResultComponent,
+    DiskCardComponent,
+    DecadeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
